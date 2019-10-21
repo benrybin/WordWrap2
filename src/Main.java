4d93aa3339b811e1bace3118520bb9c83646ead2
Ben Rybinski <45921913+benrybin@users.noreply.github.com>
2019-10-21 11:52:08 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         //System.out.println(WordWrap.wrapText("Comcast’s culture is built on integrity and respect – with the goal of working together to drive ourselves and our businesses to always be creative, innovative and competitive.",13));
 
-       WordWrap.wrapTextFromFileandOutput("C:\\Users\\Benrybin\\Documents\\GitHub\\demodb\\WordWrap2\\src\\example.txt","test.txt",13);
+       WordWrap.wrapTextFromFileandOutput("","test.txt",13);
     }
 }
 
